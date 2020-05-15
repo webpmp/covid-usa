29fc35eaf6393724b96ef0e0530cf8de86b87a73
Chris Adkins <webpmp@gmail.com>
2020-05-14 18:10:55 -0700

    increased width for dropdown and slider

--- covid-us.py
+++ covid-us.py
@@ -17,19 +17,19 @@ app = dash.Dash(__name__)
 # dropdown for state selection
 app.layout = html.Div([
     html.Div([
-        html.Label('Select States'),
+        html.Label('State'),
         dcc.Dropdown(
             id='state',
             options=[{'label': i, 'value': i} for i in states],
             value='',
-            placeholder='Select...',
+            placeholder='Select States...',
             multi=True
         )
     ],
-    style={'width': '20%', 'display': 'inline-block', 'margin-bottom': '20px'}),
+    style={'width': '25%', 'display': 'inline-block', 'margin-bottom': '20px'}),
 # slideer for filtering by number of deaths
     html.Div([
-        html.Label('Deaths Reported'),
+        html.Label('Total Deaths'),
         dcc.Slider(
             id='deaths-slider',
             min=1,
@@ -39,7 +39,7 @@ app.layout = html.Div([
             marks={'1': '>1', '500': '>500', '1000': '>1000', '1500': '>1500', '2000': '>2000', '2500': '>2500'}
         ),
     ],
-    style={'width': '20%', 'display': 'inline-block', 'margin-bottom': '20px', 'margin-left': '20px'}),
+    style={'width': '35%', 'display': 'inline-block', 'margin-bottom': '20px', 'margin-left': '20px'}),
 
     html.Div([
         dcc.Graph(id='deaths-by-date'),
@@ -82,7 +82,7 @@ def update_graph(deaths, state):
         'layout': go.Layout(
             xaxis={'title': 'Date', 'titlefont': dict(size=18, color='darkgrey'), 'zeroline': False, 'ticks': 'outside'},
             yaxis={'title': 'Total Deaths', 'titlefont': dict(size=18, color='darkgrey'), 'range': [000, 35000], 'ticks': 'outside'},
-            margin={'l': 60, 'b': 70, 't': 30, 'r': 20},
+            margin={'l': 60, 'b': 60, 't': 30, 'r': 20},
             legend={'x': 1, 'y': 1},
             hovermode='closest'
         )
